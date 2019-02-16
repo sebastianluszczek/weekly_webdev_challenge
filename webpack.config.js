dd7aea3fba9286a8f6f23cd9bd5ac56888a3e8ac
sebastianluszczek <luszczeksebastian@gmail.com>
2019-02-16 17:16:42 +0100

    initial master commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+});
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    watch: true,
+    mode: "development", //ta opcja zostanie pominięta jeżeli użyjemy npm run build
+    devtool: "source-map",
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ["env", {
+                                targets: {
+                                    browsers: ['> 1%']
+                                }
+                            }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: extractPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'resolve-url-loader', 'postcss-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png|jpeg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ]
+}
